ff241ed8a316ca93aab69e4b02335a04a34a88bc
Bernard  Mel <bernard.mamuyac@gmail.com>
2020-03-09 22:36:26 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,3 +8,10 @@ def batch_badge_creator(attendees)
     badge_maker(name)
   end
   end
+
+def assign_rooms(attendees)
+  room=0
+  attendees.collet do |name|
+    room+=1
+    "Hello, #{name}! You'll be assigned to room #{room}!"
+  end 
